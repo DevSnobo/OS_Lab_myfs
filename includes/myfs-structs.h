02d75ae9e39aa510dc7f35a359b4287aac093b51
nansaidh <melanie.wolf_3008@t-online.de>
2018-12-12 13:09:06 +0100

    Geänderte structs unvollständig

--- includes/myfs-structs.h
+++ includes/myfs-structs.h
@@ -8,15 +8,64 @@
 
 #ifndef myfs_structs_h
 #define myfs_structs_h
+#include <cstdint>
 
 #define NAME_LENGTH 255
 #define BLOCK_SIZE 512
 #define NUM_DIR_ENTRIES 64
 #define NUM_OPEN_FILES 64
 
-struct Superblock {}
-struct DMap {}
-struct FAT {}
-struct Root {}
 
 
+// TODO: Add structures of your file system here
+
+struct Superblock {
+
+    static const uint8_t BLOCKSIZE = BLOCK_SIZE;
+    static const uint8_t USABLE_BLOCKS = 61440;//30 MiB = 31457280 Byte / 512 Byte
+    static const uint8_t SUPER_BLOCK_POS = 0;
+    static const uint8_t SUPER_BLOCK_SIZE = 1;//?
+    static const uint8_t DMAP_POS = 1;//?
+    static const uint8_t DMAP_SIZE = 0;//?
+    static const uint8_t FAT_POS = 0; //?
+    static const uint8_t FAT_SIZE = 0;//?
+    static const uint8_t ROOT_POS = 0;//?
+    static const uint8_t ROOT_SIZE = 0;//?
+    static const uint8_t DATA_POS = 0;//?
+    static const uint8_t DATA_SIZE = 0;//?
+    uint8_t current_entries = 0;
+    uint8_t open_files = 0;
+
+};
+
+struct DMap {
+    //2 Blöcke mit Zahlen von 0-511 bzw 1-512
+    short[] dmap;
+}
+
+struct FAT {
+    short[512][2] fat;
+}
+
+struct file { //stat struct nehmen?
+    char *file_name;
+    short file_size;
+    char *uid;
+    char *gid;
+    short file_perm;
+
+
+    /*struct times {
+        long atime;
+        long mtime;
+        long ctime;
+    }*/
+
+    short* file_start;
+}
+
+struct root {
+    file* files[];
+}
+
+#endif /* myfs_structs_h */
